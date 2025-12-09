6946af3c887543acddde3ee95ec4ddd1448d6d54
Mohamed Amine Aydoun <mohamed.amine.aydoun@gmail.com>
2025-12-09 18:29:22 +0100

    Add Radix-based carousel component

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,29 @@
 "use client";
 
+import { useMemo } from "react";
+
+import { Carousel, CarouselFrame } from "@/components/ui/carousel";
 import { useGetUserByIdQuery } from "./services/users";
 
 export default function Home() {
   const { data } = useGetUserByIdQuery();
 
+  const frames = useMemo(
+    () => [
+      <CarouselFrame key="frame-1">First brew</CarouselFrame>,
+      <CarouselFrame key="frame-2">Second sip</CarouselFrame>,
+      <CarouselFrame key="frame-3">Third roast</CarouselFrame>,
+      <CarouselFrame key="frame-4">Fourth crema</CarouselFrame>,
+      <CarouselFrame key="frame-5">Fifth aroma</CarouselFrame>,
+    ],
+    [],
+  );
+
   console.log({ data });
+
   return (
-    <>
-      <div className="px-8 flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        main
-      </div>
-    </>
+    <div className="px-8 flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <Carousel frames={frames} />
+    </div>
   );
 }

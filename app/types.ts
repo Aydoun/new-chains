c3a60b5223541bbf02112a9dde6ac53c7eaabbba
Mohamed Amine Aydoun <mohamed.amine.aydoun@gmail.com>
2025-12-14 23:34:01 +0100

    Add collections gallery on home page

--- app/types.ts
+++ app/types.ts
@@ -4,3 +4,19 @@ export interface User {
   email: string;
   passwordHash: string;
 }
+
+export type Visibility = "PUBLIC" | "PRIVATE" | "FRIENDS_ONLY";
+export type FrameType = "PHRASE" | "IMAGE" | "VIDEO" | "AUDIO" | "DOCUMENT";
+
+export interface Collection {
+  id: number;
+  title: string;
+  description: string;
+  url?: string | null;
+  userId: number;
+  FrameOrder: number[];
+  isDeleted: boolean;
+  visibility: Visibility;
+  createdAt: string;
+  updatedAt: string;
+}
